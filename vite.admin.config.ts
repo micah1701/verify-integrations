825c8b5df6547ac7ea47d875bef87e5223cd87f0
Micah Murray <micah.murray@gmail.com>
2026-04-01 16:56:12 -0400

    fix relative pathing

--- vite.admin.config.ts
+++ vite.admin.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/bigcommerce/admin/',
   build: {
     outDir: 'dist/admin',
     rollupOptions: {
